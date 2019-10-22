69c9e324fd2a7ecc9183b59308d158cfbcfbfa4a
Ryan Sowden <Ryan@Susans-MacBook-Pro.local>
2019-10-22 20:43:38 +1000

    Added ability to add a contact

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,22 +7,21 @@ public class Main{
         static void welcome(){
             
             System.out.println("Welcome to cmdadd, please select from the below options:");
-            System.out.println("1.Login");
-            System.out.println("2.Register");
+            System.out.println("1. Login");
+            System.out.println("2. Register");
+            System.out.println("3. Exit");
 
             Scanner choice = new Scanner(System.in);
             String answer = choice.nextLine();
 
             if(answer.equals("1")) {
-
                 login.userLogin();
-
-
-            }
-
-            else if(answer.equals("2")){
+            }else if(answer.equals("2")){
                 Register.registerUser();
-            } else {
+            } else if(answer.equals("3")){
+                System.exit(0);
+            }
+            else {
                 System.out.println("Invalid Choice");
                 welcome();
             }
--- src/com/company/contacts.java
+++ src/com/company/contacts.java
@@ -1,4 +1,6 @@
 package com.company;
+import org.jcp.xml.dsig.internal.dom.DOMSubTreeData;
+
 import java.sql.*;
 import java.util.Scanner;
 
@@ -8,28 +10,72 @@ public class contacts {
 
     static void homeScreen(){
 
+
+
         System.out.println("Welcome, please chose from the follwoing options: ");
         System.out.println("1. Add a user");
         System.out.println("2. Delete a user");
         System.out.println("3. Edit a user");
         System.out.println("4. Display contacts");
+        System.out.println("5. Logout");
         String choice = userChoice.nextLine();
 
         if(choice.equals("1")) {
-            adduser();
+            addUser();
         } else if(choice.equals("2")) {
             deleteUser();
         } else if(choice.equals("3")) {
             editUser();
-        } else if(choice.equals("4")){
+        } else if(choice.equals("4")) {
             displayContacts();
+        } else if(choice.equals("5")){
+            Main.welcome();
         } else {
             System.out.println("Invalid choice, please select from the following numbers: ");
             homeScreen();
         }
     }
 
-    static void adduser(){
+    static void addUser(){
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        System.out.println("Please enter the persons name: ");
+        String name = userChoice.nextLine();
+
+        System.out.println("Please enter phone number: ");
+        String number = userChoice.nextLine();
+
+        System.out.println("Please enter email address: ");
+        String email = userChoice.nextLine();
+
+        System.out.println("Confirm adding: " + name + " " + number + " " + email +"?" + " " + "(Y/N");
+        String confirm = userChoice.nextLine();
+
+      try{
+          c = DriverManager.getConnection("jdbc:sqlite:test.db");
+
+          String query = "INSERT into contacts(Name, Phone, Email) VALUES (?,?,?)";
+          ps = c.prepareStatement(query);
+          ps.setString(1, name);
+          ps.setString(2, number);
+          ps.setString(3, email);
+
+          if (confirm.equalsIgnoreCase("N")) {
+              addUser();
+          } else if(confirm.equalsIgnoreCase("Y")) {
+
+              int i = ps.executeUpdate();
+              System.out.println("Contact successfully added");
+              homeScreen();
+          } else {
+              addUser();
+          }
+
+      } catch (Exception e) {
+          System.exit(0);
+      }
+
 
     }
 
